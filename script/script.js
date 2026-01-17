495f943bc7cc93d73b30763c2931f64748536dd1
Ahmed Gamal <ahmedgmal010909@gmail.com>
2026-01-17 07:46:38 +0200

    add Image Edit

--- script/script.js
+++ script/script.js
@@ -1,22 +1,13 @@
-var count = 1
-  // {
-  //   img: ``,
-  //   title: '',
-  //   tools: [], 
-  //   description: '',
-  //   github: "",
-  // }
-
 const projects = [
   {
-    img: `./img/projects/project${count++}.png`,
+    img: './img/projects/project1.png',
     title: 'Fruitkha',
     tools: ['PHP', 'Laravel', 'MySQL'], 
     description: 'Fruitkha is a simple and fast e-commerce platform for buying food, tech products, and daily essentials.',
     github: "https://github.com/Jeemyy/Fruitkha",
   },
   {
-    img: `./img/projects/project${count++}.png`,
+    img: './img/projects/project2.png',
     title: 'Krencel',
     tools: ['PHP', 'Laravel', 'MySQL'], 
     description: 'Krencel is a food delivery platform that allows users to order meals from restaurants quickly and conveniently.',
@@ -24,28 +15,28 @@ const projects = [
 
   },
   {
-    img: `./img/projects/project${count++}.png`,
+    img: './img/projects/project3.png',
     title: 'TimeZone',
     tools: ['PHP', 'Laravel', 'MySQL'], 
     description: 'Timezone is an e-commerce platform where users can browse and purchase watches, including luxury, sport, and classic styles.',
     github: "https://github.com/Jeemyy/TownTeam",
   },
   {
-    img: `./img/projects/project${count++}.png`,
+    img: './img/projects/project4.png',
     title: 'SignSpeak',
     tools: ['PHP', 'Laravel', 'MySQL', 'HTML', 'CSS', 'JavaScript','Bootstrap', "React js"], 
     description: 'SignSpeak is a platform that translates spoken language into sign language using a 3D model, helping communication for the deaf and mute.',
     github: "https://github.com/jeemyy/https://github.com/Jeemyy/SignSpeak_Finish",
   },
   {
-    img: `./img/projects/project${count++}.png`,
+    img: './img/projects/project5.png',
     title: 'TownTeam',
     tools: ['PHP', 'Laravel', 'MySQL', 'Database Diagram', 'JavaScript'], 
     description: 'TownTeam is an e-commerce platform for buying clothes, featuring an admin dashboard for managing products and orders.',
     github: "https://github.com/jeemyy/TownTeam",
   },
   {
-    img: `./img/projects/project${count++}.png`,
+    img: './img/projects/project6.png',
     title: 'BizConsult',
     tools: ['PHP', 'Laravel', 'MySQL', 'Database Diagram', 'JavaScript'], 
     description: 'is a comprehensive Laravel-based business consulting platform designed to help consulting firms and agencies manage their business operations efficiently. The platform features a bilingual interface (Arabic/English), a modern admin dashboard, and a responsive front-end website.',
